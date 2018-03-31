cf37bb24a1a1d9dbfef4b6e065335c34f01d4416
Vitalijus Zudenkovas <zudenkovas@gmail.com>
2018-03-31 20:16:47 +0300

    headeris su logo Prideta bootstrap ir fontawesome

--- assets/js/Header.js
+++ assets/js/Header.js
@@ -10,14 +10,9 @@ export default class Header extends Component {
     }
 
     render () {
-        return (<header>
-            <div className="logo">Logo
-            </div>
-            <nav>
-                <a href="#">Create Ads</a>
-                <a href="#">About Us</a>
-                <a href="#">Log In</a>
-                <a href="#" className="register-btn">Register</a>
+        return (<header className="container-fluid">
+            <nav className="navbar navbar-light bg-light">
+                <i className="fas fa-utensils fa-2x"></i>
             </nav>
         </header>)
     }
