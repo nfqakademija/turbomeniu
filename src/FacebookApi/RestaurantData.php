ccaab478edcf2615b7520fe25776641ffabf2155
Rokas Grigaravicius <grigroka@gmail.com>
2018-04-21 23:54:51 +0300

    doodled

--- src/FacebookApi/RestaurantData.php
+++ src/FacebookApi/RestaurantData.php
@@ -17,7 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class RestaurantData extends Controller
 {
     /**
-     * @throws FacebookSDKException
+     * @throws FacebookSDKException | FacebookResponseException
      */
     public function getRestaurantList()
     {
@@ -27,8 +27,8 @@ class RestaurantData extends Controller
             'default_graph_version' => 'v2.12',
         ]);
 
-//        TODO More bad news: Graph returned an error: (#200) Access to this data is temporarily disabled for non-active apps or apps that have not recently accessed this data due to changes we are making to the Facebook Platform. https://developers.facebook.com/status/issues/205942813488872/
-        $fb->setDefaultAccessToken('115784507324|3Dp3uKYkXxiASZdcQg-iT2BcBXM');
+//        TODO Graph returned an error: (#200) Access to this data is temporarily disabled for non-active apps or apps that have not recently accessed this data due to changes we are making to the Facebook Platform. https://developers.facebook.com/status/issues/205942813488872/
+        $fb->setDefaultAccessToken('989934814502632|WsEN-mXRwWmzX9TUy9XdnjSC_YM');
 
         try {
             $request = $fb->get(
