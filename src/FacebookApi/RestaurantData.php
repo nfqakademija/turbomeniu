5785d9e4a213e4d37e87280670dc5057747bbe68
Rokas Grigaravicius <grigroka@gmail.com>
2018-04-21 23:28:20 +0300

    fixed bad signature error, new error.

--- src/FacebookApi/RestaurantData.php
+++ src/FacebookApi/RestaurantData.php
@@ -27,11 +27,16 @@ class RestaurantData extends Controller
             'default_graph_version' => 'v2.12',
         ]);
 
-//        TODO Solve issue - Graph returned an error: Bad signature.
-        $fb->setDefaultAccessToken('');
+//        TODO More bad news: Graph returned an error: (#200) Access to this data is temporarily disabled for non-active apps or apps that have not recently accessed this data due to changes we are making to the Facebook Platform. https://developers.facebook.com/status/issues/205942813488872/
+        $fb->setDefaultAccessToken('115784507324|3Dp3uKYkXxiASZdcQg-iT2BcBXM');
 
         try {
-            $request = $fb->get('GET', 'search?q=restaurant&type=place&center=54.872996, 23.903980&distance=2000', $fb->getDefaultAccessToken());
+            $request = $fb->get(
+                'search?q=restaurant&type=place&center=54.872996, 23.903980&distance=2000',
+                $fb->getDefaultAccessToken(),
+                '',
+                $fb->getDefaultGraphVersion()
+            );
         } catch (FacebookResponseException $e) {
             // When Graph returns an error
             echo 'Graph returned an error: ' . $e->getMessage();
