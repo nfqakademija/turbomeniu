ad781ec4a307187f6c117516c3b85e2fa2a2d325
Rokas Grigaravicius <grigroka@gmail.com>
2018-05-03 19:43:22 +0300

    tested normalized json response

--- src/Controller/ReactController.php
+++ src/Controller/ReactController.php
@@ -19,20 +19,26 @@ class ReactController extends AbstractController
      */
     public function index()
     {
+        // Set id of test object.
         $id = 403;
         $restaurants = $this->getDoctrine()->getRepository(Restaurant::class)->findAll();
         $restaurant = $this->getDoctrine()->getRepository(Restaurant::class)->find($id);
-        $jsonresponse = new JsonResponse($restaurant);
 
         $encoder = new JsonEncoder();
+        // Normalizer
         $normalizer = new ObjectNormalizer();
         $dateTimeNorm = new DateTimeNormalizer();
         $normalizer->setCircularReferenceHandler(function ($restaurant) {
             return $restaurant->getId();
         });
+        // Serialized string
         $serializer = new Serializer([$dateTimeNorm, $normalizer], [$encoder]);
         $serialized = $serializer->serialize($restaurant, 'json');
+        // JsonResponse normalized test
+        $normalized = $serializer->normalize($restaurant, null);
+        $jsonresponse = new JsonResponse($normalized);
 
-        return $this->render('home/blank.html.twig', ['serialized' => $serialized, 'restaurants' => $restaurants, 'restaurant' => $restaurant, 'jsonresponse' => $jsonresponse]);
+
+        return $this->render('home/blank.html.twig', ['jsonresponse' => $jsonresponse, 'serialized' => $serialized, 'restaurants' => $restaurants, 'restaurant' => $restaurant, 'jsonresponse' => $jsonresponse]);
     }
 }
