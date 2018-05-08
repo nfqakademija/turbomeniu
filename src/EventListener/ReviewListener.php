728dea8a2c6e83249b825f62268590043c53af79
Rokas Grigaravicius <grigroka@gmail.com>
2018-05-08 11:09:18 +0300

    started working on event listener

---
+++ src/EventListener/ReviewListener.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rokas
+ * Date: 5/8/18
+ * Time: 11:03 AM
+ */
+
+namespace App\EventListener;
+
+use App\Entity\Review;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+class ReviewListener
+{
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $entityManager = $args->getEntityManager();
+
+        if ($entity instanceof Review) {
+            $restaurant = $entity->getRestaurant();
+        }
+    }
+}
