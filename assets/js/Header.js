b7d591359a14b4c8b7d9c21bb6d2231a6246140f
Vitalijus Zudenkovas <zudenkovas@gmail.com>
2018-05-25 00:47:40 +0300

    new search functionality, with clear button; saving search history funcionality with 20 last searches; styling of the header finished; some styling of the whole app;

--- assets/js/Header.js
+++ assets/js/Header.js
@@ -9,46 +9,50 @@ export default class Header extends Component {
     render() {
         return (<nav className="navbar navbar-expand-md navbar-expand-lg navbar-expand-xl navbar-dark bg-dark fixed-top header row">
 
-
-                <a className="navbar-brand" href="#"><i className="fas fa-utensils fa-2x"></i> TURBOMENIU</a>
-
-                <form className="form-inline my-2 my-lg-0 col col-md-4 col-lg-6 col-xl-7">
-                    <input className="form-control mr-sm-2 searchbar" type="search" placeholder="Search" aria-label="Search" onChange={this.props.search}/>
+                <div className="col-xs-12">
+                <a className="navbar-brand" href="#"><i className="fas fa-utensils fa-1x"></i> TURBOMENIU</a>
+                </div>
+                <form className="form-inline my-2 my-lg-0 col input-group col-xs-12" onSubmit={this.props.handleSearch}>
+                    <input className="form-control mr-sm-0 searchbar " type="search" placeholder="Search" aria-label="Search" value={this.props.searchValue} onChange={this.props.search}/>
+                    <div className="input-group-append">
+                    <button className="form-control btn btn-outline-light my-2 my-sm-0 searchbutton" type="submit" >Search</button>
+                    <button className="form-control btn btn-outline-light my-2 my-sm-0 clearbutton" type="clear" onClick={this.props.clearSearch}>Clear</button>
+                    </div>
                 </form>
 
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                {/*<button className="navbar-toggler" type="button" data-toggle="collapse"*/}
+                        {/*data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"*/}
+                        {/*aria-expanded="false"*/}
+                        {/*aria-label="Toggle navigation">*/}
+                    {/*<span className="navbar-toggler-icon"></span>*/}
+                {/*</button>*/}
 
 
-                <div className="collapse navbar-collapse col" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Artimiausi</a>
-                        </li>
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Geriausia kaina</a>
-                        </li>
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Geriausias įvertinimas</a>
-                        </li>
-                        <li className="nav-item active dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Filtruoti
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="#">Jautiena</a>
-                                <a className="dropdown-item" href="#">Vištiena</a>
-                                <a className="dropdown-item" href="#">Kiauliena</a>
-                            </div>
-                        </li>
-                    </ul>
+                {/*<div className="collapse navbar-collapse col" id="navbarSupportedContent">*/}
+                    {/*<ul className="navbar-nav mr-auto">*/}
+                        {/*<li className="nav-item active">*/}
+                            {/*<a className="nav-link" href="#">Artimiausi</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item active">*/}
+                            {/*<a className="nav-link" href="#">Geriausia kaina</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item active">*/}
+                            {/*<a className="nav-link" href="#">Geriausias įvertinimas</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item active dropdown">*/}
+                            {/*<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"*/}
+                               {/*data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">*/}
+                                {/*Filtruoti*/}
+                            {/*</a>*/}
+                            {/*<div className="dropdown-menu" aria-labelledby="navbarDropdown">*/}
+                                {/*<a className="dropdown-item" href="#">Jautiena</a>*/}
+                                {/*<a className="dropdown-item" href="#">Vištiena</a>*/}
+                                {/*<a className="dropdown-item" href="#">Kiauliena</a>*/}
+                            {/*</div>*/}
+                        {/*</li>*/}
+                    {/*</ul>*/}
 
-                </div>
+                {/*</div>*/}
 
 
                 {/*<nav className="nav navbar fixed-top">*/
